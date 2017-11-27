508860b688acfb7dc532262e1b1277bb1bef20f8
NitchananKeawthong <Nakon6888i@gmail.com>
2017-11-27 14:58:59 +0700

    TestCondition

---
+++ src/ooplab3/TestCondition.java
@@ -0,0 +1,39 @@
+package ooplab3;
+
+public class TestCondition {
+    public static void main(String[] args) {
+//  if
+        int x = 10, y = 20;
+        if (x < y){
+            System.out.println("X less than y");}
+        else{
+            System.out.println("x more than y");}
+//        if-else-if
+        if (y>30){
+            System.out.println("y less than 30");
+        }
+        else if (y>20){
+            System.out.println("y less than 20");
+        }
+        else{
+            System.out.println("y more than 20");
+            if (x+y>=30)
+                System.out.println("x+Y=30");
+        }
+
+        {
+            System.out.println("x more than y");
+        }
+        System.out.println("IS221");
+
+
+
+
+
+
+
+
+
+    }
+    }
+
