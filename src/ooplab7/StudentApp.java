543e861baa71106c87e65b12adee329e0ade12b3
NitchananKeawthong <nakon6888i@gmail.com>
2018-01-22 14:55:58 +0700

    StudentApp2

--- src/ooplab7/StudentApp.java
+++ src/ooplab7/StudentApp.java
@@ -1,5 +1,7 @@
 package ooplab7;
 
+import java.util.Scanner;
+
 public class StudentApp {
     public static void main(String[] args) {
 //create object as Student
@@ -11,12 +13,31 @@ public class StudentApp {
 
     Student s2 = new Student("222222222222","Gril Saiyai","Maketoing","MK221");
     showData(s1);
+    showData(s2);
+
+    Student s3 = new Student();
+    s3 = inputData(s3);
+    showData(s3);
 
 
 
 
     }//    main
+private static Student inputData(Student s){
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("Please enter student info:");
+    System.out.println("Enter student ID: ");
+    s.setId(scanner.nextLine());
+    System.out.println("Enter student name: ");
+    s.setName(scanner.nextLine());
+    System.out.println("Enter student major: ");
+    s.setMajor(scanner.nextLine());
+    System.out.println("Enter student group: ");
+    s.setGroup(scanner.nextLine());
 
+    return s;
+//    inputData
+}
     private static void showData(Student s) {
         System.out.println("Student info:");
         System.out.println("ID"+s.getId());
@@ -24,7 +45,7 @@ public class StudentApp {
         System.out.println("Major"+s.getMajor());
         System.out.println("Group"+s.getGroup());
 
-    }
+    }//    shoeData
 
 
 }//class
