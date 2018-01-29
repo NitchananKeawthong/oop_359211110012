e3e02988c79512d89d424b1dc733f17cac30910c
NitchananKeawthong <nakon6888i@gmail.com>
2018-01-29 14:54:10 +0700

    Pet

---
+++ src/ooplab8/Pet.java
@@ -0,0 +1,45 @@
+package ooplab8;
+
+public class Pet {
+    private String name;
+    private String harircolor;
+    private String gender;
+    private int age;
+
+    public void makeNoise(){}
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", harircolor='" + harircolor + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Pet(String name, String harircolor, String gender, int age) {
+        this.name = name;
+        this.harircolor = harircolor;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHarircolor() {
+        return harircolor;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+//         getter and setter methor
+
+}
